bebcb6e6d931e57cafe4271908deb5eb742e6cf5
LeeKing2017 <thrusum123@gmail.com>
2024-01-21 13:48:05 +0900

    FEAT<JasyptConfig>:jasypt 암호화 적용

---
+++ core/src/main/java/com/wemingle/core/global/config/jasypt/JasyptConfig.java
@@ -0,0 +1,23 @@
+package com.wemingle.core.global.config.jasypt;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JasyptConfig {
+
+    @Bean
+    public StringEncryptor encryptor() {
+        PooledPBEStringEncryptor pooledPBEStringEncryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig simpleStringPBEConfig = new SimpleStringPBEConfig();
+        simpleStringPBEConfig.setPassword(System.getProperty("jasypt.encryptor.password"));
+        simpleStringPBEConfig.setAlgorithm("PBEWithMD5AndDES");
+        simpleStringPBEConfig.setKeyObtentionIterations(1000);
+        simpleStringPBEConfig.setPoolSize(4);
+        pooledPBEStringEncryptor.setConfig(simpleStringPBEConfig);
+        return pooledPBEStringEncryptor;
+    }
+}
