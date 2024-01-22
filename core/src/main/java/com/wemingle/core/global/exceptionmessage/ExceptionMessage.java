f1fe2b91c9793e3268b73693ed2b385d9840ca28
jang-199 <jw1010110@gmail.com>
2024-01-22 12:09:43 +0900

    FEAT: 대학별 도메인 검증을 위한 로직 추가

--- core/src/main/java/com/wemingle/core/global/exceptionmessage/ExceptionMessage.java
+++ core/src/main/java/com/wemingle/core/global/exceptionmessage/ExceptionMessage.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ExceptionMessage {
-    MEMBER_NOT_FOUNT("사용자를 찾을 수 없습니다");
+    MEMBER_NOT_FOUNT("사용자를 찾을 수 없습니다"),
+    UNIV_DOMAIN_NOT_FOUND("미지원 대학 도메인입니다.");
     private final String exceptionMessage;
 }
