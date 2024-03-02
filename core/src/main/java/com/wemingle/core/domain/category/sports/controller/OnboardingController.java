99858c27d62b9340a5bee9221f6a8d95e5a05b6c
jang-199 <jw1010110@gmail.com>
2024-03-02 19:53:01 +0900

    feat<OnboardingController>: controller 서비스 로직 추가되면 추후 업데이트 예정

--- core/src/main/java/com/wemingle/core/domain/category/sports/controller/OnboardingController.java
+++ core/src/main/java/com/wemingle/core/domain/category/sports/controller/OnboardingController.java
@@ -1,5 +1,6 @@
 package com.wemingle.core.domain.category.sports.controller;
 
+import com.wemingle.core.domain.category.sports.dto.OnboardDto;
 import com.wemingle.core.global.responseform.ResponseHandler;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,7 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class OnboardingController {
 
     @PostMapping("/")
-    ResponseEntity<ResponseHandler<?>> setOnboardInfo(@RequestBody) {
+    ResponseEntity<ResponseHandler<Object>> setOnboardInfo(@RequestBody OnboardDto.RequestOnboardInfoDto onboardInfoDto) {
+        return ResponseEntity.ok(
+                ResponseHandler.builder()
+                        .responseMessage("Onboard update completed")
+                        .build()
+        );
 
     }
 }
--- core/src/main/java/com/wemingle/core/domain/category/sports/dto/OnboardDto.java
+++ core/src/main/java/com/wemingle/core/domain/category/sports/dto/OnboardDto.java
@@ -1,11 +1,14 @@
 package com.wemingle.core.domain.category.sports.dto;
 
 
+import com.wemingle.core.domain.category.sports.entity.sportstype.Sportstype;
 import lombok.Value;
+
+import java.util.List;
 
 public class OnboardDto {
     @Value
     public static class RequestOnboardInfoDto{
-
+        List<Sportstype> selectedSports;
     }
 }
