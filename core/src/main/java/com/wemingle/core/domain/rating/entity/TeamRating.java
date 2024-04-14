3883e32631c5911f1ffd49a708168584efb31c47
LeeKing2017 <thrusum123@gmail.com>
2024-04-14 19:22:24 +0900

    feat<MatchingRequestController>: 매칭 신청 받은 기록 조회 api 구현 * 팀으로 매칭 신청 받은 기록 조회 api * 개인으로 매칭 신청 받은 기록 조회 api

--- core/src/main/java/com/wemingle/core/domain/rating/entity/TeamRating.java
+++ core/src/main/java/com/wemingle/core/domain/rating/entity/TeamRating.java
@@ -3,10 +3,10 @@ package com.wemingle.core.domain.rating.entity;
 import com.wemingle.core.domain.team.entity.Team;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
-
-import java.math.BigDecimal;
+import lombok.Getter;
 
 @Entity
+@Getter
 public class TeamRating {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "PK")
@@ -14,7 +14,7 @@ public class TeamRating {
 
     @NotNull
     @Column(name = "TOTAL_RATING")
-    private BigDecimal totalRating;
+    private Double totalRating;
 
     @NotNull
     @ManyToOne(fetch = FetchType.LAZY)
---
+++ core/src/main/java/com/wemingle/core/domain/rating/service/TeamRatingService.java
@@ -0,0 +1,4 @@
+package com.wemingle.core.domain.rating.service;
+
+public interface TeamRatingService {
+}
