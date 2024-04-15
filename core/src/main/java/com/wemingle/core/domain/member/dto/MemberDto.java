092c341ac05a6d5ed105ca4b80bf43941401c9ef
LeeKing2017 <thrusum123@gmail.com>
2024-04-16 01:56:48 +0900

    style:todo 추가

--- core/src/main/java/com/wemingle/core/domain/member/dto/MemberDto.java
+++ core/src/main/java/com/wemingle/core/domain/member/dto/MemberDto.java
@@ -25,6 +25,7 @@ public class MemberDto {
     public static class MemberInfoInSearch{
         private String nickname;
         private String profileImg;
+        //todo 채팅구현됐을 시 1대1대화방 어떤식으로 데이터 줄건 지 확인 후 추가 구현
 
         @Builder
         public MemberInfoInSearch(String nickname, String profileImg) {
