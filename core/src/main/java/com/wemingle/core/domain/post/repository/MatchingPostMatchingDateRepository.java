05cc434c7952cc35982bb16bfce788c8842d5fde
LeeKing2017 <thrusum123@gmail.com>
2024-05-04 05:28:16 +0900

    refactor<MatchingPostService>: 매칭글 다시 올리기 서비스 로직 수정 * 기존의 매칭글을 삭제하고 다시 올리는 로직으로 수정

---
+++ core/src/main/java/com/wemingle/core/domain/post/repository/MatchingPostMatchingDateRepository.java
@@ -0,0 +1,7 @@
+package com.wemingle.core.domain.post.repository;
+
+import com.wemingle.core.domain.post.entity.MatchingPostMatchingDate;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MatchingPostMatchingDateRepository extends JpaRepository<MatchingPostMatchingDate,Long> {
+}
