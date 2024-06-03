a430c2988b873ac212e1e2ad9f01f6d81ddcfa76
LeeKing2017 <thrusum123@gmail.com>
2024-06-04 03:28:20 +0900

    refactor<NicknameService>: 닉네임 사용 가능한지 확인하는 로직 수정 * 팀에 가입 신청했을 때 정한 닉네임들에도 해당 닉네임이 있는 지 확인

--- core/src/main/java/com/wemingle/core/domain/matching/repository/TeamRequestRepository.java
+++ core/src/main/java/com/wemingle/core/domain/matching/repository/TeamRequestRepository.java
@@ -3,11 +3,13 @@ package com.wemingle.core.domain.matching.repository;
 import com.wemingle.core.domain.matching.entity.TeamRequest;
 import com.wemingle.core.domain.member.entity.Member;
 import com.wemingle.core.domain.team.entity.Team;
+import com.wemingle.core.domain.team.entity.TeamMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TeamRequestRepository extends JpaRepository<TeamRequest, Long> {
     boolean existsByTeamAndRequester(Team team, Member requester);
@@ -15,4 +17,5 @@ public interface TeamRequestRepository extends JpaRepository<TeamRequest, Long> 
     List<TeamRequest> findByTeamFetchMember(@Param("team") Team team);
     @Query("select count(*) from TeamRequest tr where tr.team.pk = :teamPk")
     Integer findPendingTeamRequestByTeamCnt(@Param("teamPk") Long teamPk);
+    Optional<TeamMember> findByTeam_PkAndNickname(Long teamPk, String nickname);
 }
