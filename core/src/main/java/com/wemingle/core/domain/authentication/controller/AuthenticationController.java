82df15914130d0ef8f0af019925c58f9d3545abb
LeeKing2017 <thrusum123@gmail.com>
2024-06-07 21:49:40 +0900

    refactor<AuthenticationController>:토큰 재발급 api refactor

--- core/src/main/java/com/wemingle/core/domain/authentication/controller/AuthenticationController.java
+++ core/src/main/java/com/wemingle/core/domain/authentication/controller/AuthenticationController.java
@@ -11,57 +11,33 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import static com.wemingle.core.global.exceptionmessage.ExceptionMessage.IS_EXPIRED_REFRESH_AND_ACCESS_TOKEN;
-
 @RestController
 @RequiredArgsConstructor
 @Slf4j
 public class AuthenticationController {
     private final TokenService tokenService;
 
+    /**
+     * accessToken -> 무조건 재발급
+     * refreshToken -> 만료 21일 전에만 재발급
+     */
     @PatchMapping("/token")
-    public ResponseEntity<?> updateToken(@RequestBody TokenDto.RequestTokenDto requestTokenDto){
-        String refreshToken = requestTokenDto.getRefreshToken();
-        String accessToken = requestTokenDto.getAccessToken();
-
-        if (tokenService.verifyRefreshAndAccessToken(refreshToken, accessToken)){
-            if (tokenService.isExpiredRefreshAndAccessToken(refreshToken, accessToken)){
-                return ResponseEntity
-                        .status(HttpStatus.FORBIDDEN)
-                        .body(ResponseHandler.<String>builder()
-                            .responseMessage(IS_EXPIRED_REFRESH_AND_ACCESS_TOKEN.getExceptionMessage())
-                            .responseData(null)
-                            .build()
-                        );
+    public ResponseEntity<?> createNewTokens(@RequestBody TokenDto.RequestTokenDto requestTokenDto){
+        if (tokenService.isSignInRequired(requestTokenDto)){
+            return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                    .body(ResponseHandler.<String>builder()
+                        .responseMessage("Please sign in again")
+                        .build()
+                    );
             }
-        }
 
-        TokenDto.ResponseTokenDto.ResponseTokenDtoBuilder responseTokenDtoBuilder = TokenDto.ResponseTokenDto.builder();
-        if (tokenService.verifyRefreshToken(refreshToken)) {
-            String newAccessToken = tokenService.createAccessTokenByRefreshToken(refreshToken);
-            responseTokenDtoBuilder.accessToken(newAccessToken);
-            responseTokenDtoBuilder.accessTokenExpiredTime(tokenService.getExpirationTime(newAccessToken));
-
-
-            if (tokenService.isExpiredAfter21Days(refreshToken)) {
-                String newRefreshToken = tokenService.createAndPatchRefreshTokenInMember(refreshToken);
-                responseTokenDtoBuilder.refreshToken(newRefreshToken);
-                responseTokenDtoBuilder.refreshTokenExpiredTime(tokenService.getExpirationTime(newRefreshToken));
-            }
-        }
-
-
-        TokenDto.ResponseTokenDto responseTokenDto = responseTokenDtoBuilder.build();
+        TokenDto.ResponseTokenDto responseData = tokenService.createNewTokens(requestTokenDto.getRefreshToken());
 
         return ResponseEntity
                 .ok(ResponseHandler.<TokenDto.ResponseTokenDto>builder()
-                        .responseMessage(generateResponseMessage(responseTokenDto))
-                        .responseData(responseTokenDto)
+                        .responseMessage("Issuance tokens successfully")
+                        .responseData(responseData)
                         .build()
                 );
-    }
-
-    private static String generateResponseMessage(TokenDto.ResponseTokenDto responseTokenDto){
-        return responseTokenDto.getRefreshToken() == null ? "Reissuance Access Token Completed" : "Reissuance Refresh and Access Token completed";
     }
 }
